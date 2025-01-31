ff52ff2ce1c8c6b4078cb3ed2b62f36397816fe5
Water <507140388@qq.com>
2025-01-31 22:56:50 +0800

    token的注入

--- src/utils/request.js
+++ src/utils/request.js
@@ -1,4 +1,5 @@
 import axios from 'axios'
+import { getToken } from './token'
 
 const request = axios.create({
   baseURL: 'http://geek.itheima.net/v1_0',
@@ -13,10 +14,16 @@ request.interceptors.request.use((config)=> {
 })
 
 // 添加响应拦截器
-request.interceptors.response.use((response)=> {
-    // 2xx 范围内的状态码都会触发该函数。
-    // 对响应数据做点什么
-    return response.data
+// 在请求发送之前 做拦截 插入一些自定义的配置 [参数的处理]
+request.interceptors.response.use((config)=> {
+    // 操作这个config 注入token数据
+    // 1. 获取到token
+    // 2. 按照后端的格式要求去做token拼接
+    const token = getToken()
+    if (token) {
+      config.headers.Authorization = `Bearer ${token}`
+    }
+    return config
   }, (error)=> {
     // 超出 2xx 范围的状态码都会触发该函数。
     // 对响应错误做点什么
